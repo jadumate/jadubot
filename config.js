a490f3ef58d23a01570865fd8301bf5f4963e974
Jadumate Park <jadumate@gmail.com>
2017-11-28 20:02:02 +0900

    core update

---
+++ config.js
@@ -0,0 +1,8 @@
+
+
+var config = { 
+   apiToken : 'xoxb-277692464883-ibwL6K6LmQoG......',
+   urlWebhookGeneral: 'https://hooks.slack.com/services/T85..../B85PN.../DqljY1n132.....',
+   botName : 'jadubot'
+};
+
